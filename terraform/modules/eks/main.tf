0b7a56d866a1080d4bbfe42aa874be9207eabbc8
Ed Pham <nghiabn1990@gmail.com>
2023-11-11 17:44:49 +0700

    fix bug

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -124,7 +124,7 @@ module "eks" {
   } : {}
 
   # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
+  eks_managed_node_group_defaults = var.eks_type == "eks_managed" ? {
     ami_type       = "${var.ami_type}"
     instance_types = "${var.instance_types}"
 
@@ -134,7 +134,7 @@ module "eks" {
       additional               = aws_iam_policy.additional.arn
       AmazonEBSCSIDriverPolicy = var.create_ebs_csi_driver ? "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy" : null
     }
-  }
+  } : {}
 
   eks_managed_node_groups = (var.eks_type == "eks_managed") ? {
     # blue = {}
